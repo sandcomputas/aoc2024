440c76e0f5dc6012ef600e9c74ef10b3f373f74c
Sondre Andersen <sand@computas.com>
2024-11-25 11:44:53 +0100

    created possibility to run specific env and part

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -11,14 +11,22 @@ class Day(
         parts.forEach { runAndPrint(it) }
     }
 
-    fun part(part: PartEnum) {
-        val partsToRun = if (part == PartEnum.ALL) parts else parts.filter { it.part() == part.i}
-        partsToRun.forEach { runAndPrint(it)}
+    fun part(part: PartEnum, env: Env? = null) {
+        val partsToRun = if (part == PartEnum.ALL) parts else parts.filter { it.part() == part.i }
+        partsToRun.forEach { runAndPrint(it, env) }
     }
 
-    private fun runAndPrint(part: Part) {
-        part.test()
-        part.actual()
+    private fun runAndPrint(part: Part, env: Env? = null) {
+        val envs: MutableList<() -> Unit> = mutableListOf()
+        when (env) {
+            Env.TEST -> envs.add(part::test)
+            Env.ACTUAL -> envs.add(part::actual)
+            null -> {
+                envs.add(part::test)
+                envs.add(part::actual)
+            }
+        }
+        envs.forEach { it.invoke() }
         part.run()
         println(part)
     }
--- src/main/kotlin/day01/Main.kt
+++ src/main/kotlin/day01/Main.kt
@@ -1,8 +1,11 @@
 package dev.sondre.day01
 
 import dev.sondre.Day
+import dev.sondre.Env
+import dev.sondre.PartEnum
 
 fun main() {
     val day = Day(listOf(Part1(), Part2()))
-    day.all()
-}    
+//    day.all()
+    day.part(PartEnum.ONE, Env.TEST)
+}

6a90d97e5f0e995496578014dc9905a1f054bb09
Sondre Andersen <sand@computas.com>
2024-11-30 00:00:13 +0100

    small update to extension functions

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,11 +1,11 @@
 package dev.sondre
 
 fun <T> Iterable<T>.skipN(n: Int): Iterable<T> {
-    return this.filterIndexed { i, _ -> i != n}
+    return this.filterIndexed { i, _ -> i != n } // Might not be super efficient, .drop(n) might be better
 }
 
 fun <T> Iterable<T>.skipFirst(): Iterable<T> {
-    return this.skipN(0)
+    return this.drop(1)
 }
 
 fun Iterable<Int>.productOf(): Int {

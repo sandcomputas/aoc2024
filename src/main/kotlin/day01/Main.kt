65402aee7146eecf01babf9417a6818264353f06
Sondre Andersen <sand@computas.com>
2024-12-01 12:33:27 +0100

    delete old day1

--- src/main/kotlin/day01/Main.kt
+++
@@ -1,9 +0,0 @@
-package dev.sondre.day01
-
-import dev.sondre.Day
-
-fun main() {
-    val day = Day(listOf(Part1(42), Part2(42)))
-    day.all()
-//    day.part(PartEnum.ONE, Env.TEST)
-}
--- src/main/kotlin/day01/Part2.kt
+++
@@ -1,9 +0,0 @@
-package dev.sondre.day01
-
-import dev.sondre.Part
-
-class Part2(expRes: Int? = null) : Part(expRes) {
-    override fun calc(data: String): Int {
-        return 42
-    }
-}            
